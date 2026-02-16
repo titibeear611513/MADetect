981dfefa60be7b020754ceb33c42cdeb4e2545d1
hsiuwei <titibear611513@gmail.com>
2026-02-16 22:44:24 +0800

    chore: remove unneceesary comment

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = Config.SECRET_KEY
     
-    # 啟用 CORS（如果可用）
+    # 啟用 CORS
     if CORS_AVAILABLE:
         CORS(app, supports_credentials=True)
     
